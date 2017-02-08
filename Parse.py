7bf811b6f864654661562b4ab44b3dfb91146ac6
Anushri Adhia <Anushri Adhia>
2017-02-07 20:30:36 -0500

    changed parse to recognize out of order commands. added a simple mailbox parser, and added a method to write messages to rcpt to ricipients

--- Parse.py
+++ Parse.py
@@ -1,82 +1,94 @@
 
-def parse_mail_from_command(line):
-    global index
 
-    index = 0
+def parse_line(line, cmd_id):
 
-    if line[:4] == "MAIL":
-        index += 4
+    if cmd_id == "MF":
+        parse_mail_from_command(line)
+    elif cmd_id == "RT":
+        parse_rcpt_to_command(line)
 
-    else:
-        print("500 Syntax error: command unrecognized")
+    if not path(line):
         return False
 
-    startind = index
+    if not crlf(line[index]):
+        if char(line[index]) or line[index].isspace() or special(line[index]):
+            return False
+        else:
+            exit()
 
-    whitespace(line)              #allows for as much whitespace and updates the index
-
-    if startind==index:
+    return True
 
 
-    if line[index:index+4] == "FROM:":
-        index += 5
-
+def parse_command(line):
+    if parse_mail_from_command(line):
+        return "MF"
+    elif parse_rcpt_to_command(line):
+        return "RT"
+    elif data_command_parse(line):
+        return "DT"
     else:
-        print("500 Syntax error: command unrecognized")
+        return "500"
+
+
+def data_command_parse(line):
+    global index
+    index = 0
+
+    if line[:4] == "DATA":
+        index += 4
+    else:
         return False
 
     whitespace(line)
 
-    if not path(line):                         #once it recieves any error,
+    return crlf(line[index])
+
+
+def parse_mail_from_command(line):
+    global index
+    index = 0
+
+    if line[:4] == "MAIL":
+        index += 4
+    else:
         return False
 
-    if not crlf(line[index]):
-        if char(line[index]) or sp(line[index]) or special(line[index]):            #makes sure there aren't any other characters after the path
-            print "500 Syntax error: command unrecognized"
-            return False
+    startind = index
+    whitespace(line)
+    if startind == index:
+        return False
 
-        else:
-            exit()                      #indicates the end of file, so exit code
+    if line[index:index+4] == "FROM:":
+        index += 5
+    else:
+        return False
 
-    print "250 OK"                   #passes everything
+    whitespace(line)
+
     return True
 
 
 def parse_rcpt_to_command(line):
     global index
-
     index = 0
 
     if line[:4] == "RCPT":
         index += 4
-
     else:
-        print("500 Syntax error: command unrecognized")
         return False
 
-    whitespace(line)              #allows for as much whitespace and updates the index
+    startind = index
+    whitespace(line)
+    if startind == index:
+        return False
 
     if line[index:index+4] == "TO:":
         index += 3
-
     else:
-        print("500 Syntax error: command unrecognized")
         return False
 
     whitespace(line)
 
-    if not path(line):                         #once it recieves any error,
-        return False
-
-    if not crlf(line[index]):
-        if char(line[index]) or sp(line[index]) or special(line[index]):            #makes sure there aren't any other characters after the path
-            print "500 Syntax error: command unrecognized"
-            return False
-
-        else:
-            exit()                      #indicates the end of file, so exit code
-
-    print "250 OK"                   #passes everything
     return True
 
 
@@ -90,41 +102,31 @@ def whitespace(line):
 
 def path(line):
     global index
-    global address
-    global startind
-    global endind
 
     if line[index] == "<":
         index += 1
     else:
-        print "501 Syntax error in parameters or arguments"
         return False
-
-    startind=index
 
     if not mailbox(line):
         return False
-
-    endind=index
 
     if line[index] == ">":
         index += 1
         return True
     else:
-        print "501 Syntax error in parameters or arguments"
         return False
 
 
 def mailbox(line):
     global index
 
-    if not local_part(line):
+    if not is_string(line, 0):
         return False
 
     if line[index] == "@":
         index += 1
     else:
-        print "501 Syntax error in parameters or arguments"
         return False
 
     if not domain(line):
@@ -135,13 +137,8 @@ def mailbox(line):
 
 def domain(line):
     global index
-    #check to see if there is element
-    #if not, check if .
-        #if there is period, call domain
-        #if not, check how many times it has recurse, if at least once return true else return false
 
     if not name(line):
-        print "501 Syntax error in parameters or arguments"
         return False
     else:
         if line[index] == ".":
@@ -151,21 +148,10 @@ def domain(line):
             return True
 
 
-def local_part(line):
-
-    if not is_string(line, 0):
-        print "501 Syntax error in parameters or arguments"
-        return False
-    else:
-        return True
-
-
 def name(line):
     global index
-    #has to start with a letter
-    #then go to letter digit string
 
-    if not a(line[index]):
+    if not (line[index]).isalpha():
         return False
     index += 1
     if not let_dig_str(line, 0):
@@ -176,12 +162,8 @@ def name(line):
 
 def let_dig_str(line, count):
     global index
-    #check for let-dig
-        #if not there, check how many times it has recursed:
-            #if more than one, return true, else return false
-    #if there, increment index and call let-dig-str again
 
-    if let_dig(line[index + count]):
+    if line[index + count].isalpha() | unicode(line[index + count].isnumeric()):
         count += 1
         return let_dig_str(line, count)
 
@@ -193,22 +175,8 @@ def let_dig_str(line, count):
             return False
 
 
-def let_dig(character):
-    global index
-
-    if a(character) | d(character):
-        return True
-    else:
-        return False
-
-
 def is_string(line, count):
     global index
-    #check for character
-        #if there, increment count , call isString function again
-        #if not, check to see count
-            #if count is > 0, return true
-            #else return false
 
     if char(line[index + count]):
         count += 1
@@ -219,23 +187,11 @@ def is_string(line, count):
 
 
 def char(character):
-    return not special(character) and not sp(character)
+    return not special(character) or character.isspace()
 
 
 def crlf(line):
     return "\n" in line or "\r" in line
-
-
-def sp(character):
-    return character.isspace()
-
-
-def a(character):
-    return character.isalpha()
-
-
-def d(character):
-    return unicode(character).isnumeric()
 
 
 def special(character):
--- SMTP1.py
+++ SMTP1.py
@@ -1,71 +1,109 @@
 import sys
 import Parse
 
-
-def main():
-
-    mail_messages()
-
 # do we have to keep stdin open after the period?? for another message?
 # how do we know the client is done sending?
-        #check for end of file; but keep on recieving until then (control + D)
+# check for end of file; but keep on receiving until then (control + D)
 # if something throws an error, should we just leave the entire thing?
-        #don't exit, either wait for current command or start message all over, either way program should be ready to read input
-# how to check if something is out of order
-        #call a universal parse command that checks what it is and have it differentiate
-        #for data, mail from, rcpt to commands
-# how to just get mailbox
-
+# don't exit, either wait for current command or start message all over, be ready to read input
+# priority of errors: syntax, out of order, the arguments
+# what is null space? after data can there be white space?
 
 
 def mail_messages():
 
-    mail_from = sys.stdin.readline()
-    if not Parse.parse_mail_from_command(mail_from):
+    mail = sys.stdin.readlines()
+    mfcode = Parse.parse_command(mail[0])
+
+    if mfcode == "500":
+        print "500 Syntax error: command unrecognized"
         return False
+    elif mfcode == "MF":
+        print "250 OK"
+    else:
+        print "503 Bad sequence of commands"
 
     recipients = []
     counter = 0
+    index = 1
 
     while True:
-        read_line = sys.stdin.readline()
+        rcptcode = Parse.parse_command(mail[index])
 
-        if read_line == "DATA":
-            print "354 Start mail input; end with <CLRF>.<CLRF>"
-            break
-
-        if not Parse.parse_rcpt_to_command(read_line):
+        if rcptcode == "500":
+            print "500 Syntax error: command unrecognized"
+            return False
+        elif rcptcode == "DT":
+            if counter == 0:
+                print "503 Bad sequence of commands"
+                return False
+            else:
+                index += 1
+                break
+        elif rcptcode == "MF":
+            print "503 Bad sequence of commands"
             return False
 
-        recipients[counter] = read_line
+        if not Parse.parse_line(mail[index], rcptcode):
+            print "501 Syntax error in parameters or arguments"
+            return False
+
+        print "250 OK"
+        recipients[counter] = mail[index]
         counter += 1
+        index += 1
 
     data_message = []
     counter = 0
 
     while True:
-        read_line = sys.stdin.readline()
-        print read_line
 
-        if end_line(read_line):
+        if end_line(mail[index]):
+            print mail[index]
             print "250 OK"
             break
 
-        data_message[counter] = read_line
+        data_message[counter] = mail[index]
         counter += 1
+        index += 1
 
-    write_to_files(recipients,  data_message)
+    write_to_files(recipients,  data_message, mail[0])
+    return True
 
 
 def end_line(line):
     return Parse.crlf(line[0]) and line[1] == "." and Parse.crlf(line[2])
 
 
-def write_to_files(recipients, data_message):
+def write_to_files(recipients, data_message, mailfrom):
 
     for address in recipients:
-        filename = "forward/" + address
+        filename = "forward/" + mailbox(address)
 
         with open(filename) as f:
+
+            f.write("From: " + mailbox(mailfrom))
+
+            for line in recipients:
+                f.write("To: " + mailbox(line))
+
             for line in data_message:
                 f.write(line)
+
+
+def mailbox(line):
+
+    ind = 0;
+
+    while not line[ind] == "<":
+        ind += 1
+
+    startind = ind
+
+    while not line[ind] == ">":
+        ind += 1
+
+    return line[startind+1:ind]
+
+
+
